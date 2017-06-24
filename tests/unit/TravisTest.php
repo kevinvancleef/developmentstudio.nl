a2b2cb806378e705312c5ae4a737f30b4b566101
Kevin van Cleef <kevin@developmentstudio.nl>
2017-06-24 14:53:09 +0200

    Setup Travis

---
+++ tests/unit/TravisTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace DevelopmentStudio\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TravisTest extends TestCase
+{
+    public function testTravis()
+    {
+        $this->assertTrue(true);
+    }
+}
